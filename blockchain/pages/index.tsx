bd207a14367ccd68b38464d04adeabc085ad0365
pranotoism <bayupranotodwiputra@gmail.com>
2022-08-08 22:20:19 +0700

    MVP

--- blockchain/pages/index.tsx
+++ blockchain/pages/index.tsx
@@ -44,7 +44,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://github.com/pranotoism/blockchain">Pranoto Blockchain!</a>
+          Welcome to <a href="https://github.com/pranotoism/blockchain">Blockchain!</a>
         </h1>
 
         <p className={styles.description}>
@@ -56,7 +56,7 @@ const Home: NextPage = () => {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/pranotoism/blockchain"
           target="_blank"
           rel="noopener noreferrer"
         >
